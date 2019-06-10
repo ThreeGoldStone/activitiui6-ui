eacfd143b7918e8586ea771ce6e76796b0a9aaeb
段金磊 <3001720396@qq.com>
2019-06-10 13:06:11 +0800

    上传activiti6的静态文档

--- activiti-app-rest/src/main/java/org/activiti/app/rest/editor/JavaServicesResource.java
+++ activiti-app-rest/src/main/java/org/activiti/app/rest/editor/JavaServicesResource.java
@@ -13,9 +13,15 @@
 package org.activiti.app.rest.editor;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,20 +29,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.yw56.javaservice.JavaServiceEntity;
 
 /**
- * 
  * @author djl20
- *
  */
 @RestController
 public class JavaServicesResource {
 
-	private final Logger log = LoggerFactory.getLogger(JavaServicesResource.class);
+    private final Logger log = LoggerFactory.getLogger(JavaServicesResource.class);
+    @Autowired
+    protected RepositoryService repositoryService;
+
+    @Autowired
+    protected HistoryService historyService;
+
+    @Autowired
+    protected RuntimeService runtimeService;
+
 //
 //	@Inject
 //	protected ObjectMapper objectMapper;
 
-	@RequestMapping(value = "/rest/java/services", method = RequestMethod.GET, produces = "application/json")
-	public ArrayList<JavaServiceEntity> getJavaserviceList() {
+    @RequestMapping(value = "/rest/java/services", method = RequestMethod.GET, produces = "application/json")
+    public ArrayList<JavaServiceEntity> getJavaserviceList() {
 //		try {
 //			String fileName = "java-services.js";
 //			JsonNode stencilNode = objectMapper
@@ -46,10 +59,11 @@ public class JavaServicesResource {
 //			log.error("Error reading bpmn stencil set json", e);
 //			throw new InternalServerErrorException("Error reading bpmn stencil set json");
 //		}
-		return JavaServiceEntity.getData();
-	}
-	@RequestMapping(value = "/rest/java/ExtServices", method = RequestMethod.GET, produces = "application/json")
-	public ArrayList<JavaServiceEntity> getExtJsServiceList() {
+        return JavaServiceEntity.getData();
+    }
+
+    @RequestMapping(value = "/rest/java/ExtServices", method = RequestMethod.GET, produces = "application/json")
+    public ArrayList<JavaServiceEntity> getExtJsServiceList() {
 //		try {
 //			String fileName = "java-services.js";
 //			JsonNode stencilNode = objectMapper
@@ -59,6 +73,24 @@ public class JavaServicesResource {
 //			log.error("Error reading bpmn stencil set json", e);
 //			throw new InternalServerErrorException("Error reading bpmn stencil set json");
 //		}
-		return JavaServiceEntity.getData();
-	}
+        return JavaServiceEntity.getData();
+    }
+
+    @RequestMapping(value = "/rest/java/getPDs", method = RequestMethod.GET, produces = "application/json")
+    public List<ProcessDefinition> getProcessDefines() {
+//		try {
+//			String fileName = "java-services.js";
+//			JsonNode stencilNode = objectMapper
+//					.readTree(this.getClass().getClassLoader().getResourceAsStream(fileName));
+//			return stencilNode;
+//		} catch (Exception e) {
+//			log.error("Error reading bpmn stencil set json", e);
+//			throw new InternalServerErrorException("Error reading bpmn stencil set json");
+//		}
+//        repositoryService.ge
+//        runtimeService
+        List<ProcessDefinition> list = repositoryService.createProcessDefinitionQuery().list();
+
+        return list;
+    }
 }
